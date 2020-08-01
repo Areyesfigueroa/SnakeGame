5805880aa4f0f39f061891b0958f5fd73c61bf2a
Aliel Reyes <alielreyes@gmail.com>
2020-07-31 17:16:54 -0700

    Snake is moving and rotating using the arrow keys

--- snakeGame.js
+++ snakeGame.js
@@ -30,7 +30,7 @@ image.src = './snake-sprite.png';
 
 let rotateSpeed=0;
 let rotateTo=0;
-
+let moveSpeed=0;
 const setArrowKeys = (active) => {
     for(key in arrowKeys) {
         arrowKeys[key].active = active;
@@ -49,22 +49,20 @@ const rotateSnakeHead = (angleSpeed, destAngle) => {
     // console.log(snake.angle,destAngle);
 
     if(angleSpeed > 0) {
-        console.log("right");
+        // console.log("right");
         if(snake.angle >= destAngle) {
             snake.angle = destAngle;
         } else {
             snake.angle += angleSpeed;
         }
     } else {
-        console.log("left");
+        // console.log("left");
         if(snake.angle <= destAngle) {
             snake.angle = destAngle;
         } else {
             snake.angle += angleSpeed;
         }
     }
-
-
 
 
     //Rotate Snake Head
@@ -87,8 +85,29 @@ const draw = () => {
     canvasContext.fillStyle = "green";
     canvasContext.fillRect(0,0,canvas.width, canvas.height);
 
+    //Update Rotation
     rotateSnakeHead(rotateSpeed, rotateTo);
 
+    //Update Movement
+    switch(snake.facingDirection) {
+        case DIRECTIONS.RIGHT:
+            snake.posX += moveSpeed;
+            break;
+        case DIRECTIONS.LEFT:
+            snake.posX -= moveSpeed;
+            break;
+        case DIRECTIONS.DOWN:
+            snake.posY += moveSpeed;
+            break;
+        case DIRECTIONS.UP:
+            snake.posY -= moveSpeed;
+            break;
+        default:
+            moveSpeed = 0;
+    }
+
+    console.log(snake.facingDirection);
+    // canvasContext.drawImage(image, snake.posX, snake.posY, snake.width, snake.height);
 
     // console.log(rotateSpeed);
     // if(isSnakeRotating()) return;
@@ -116,23 +135,83 @@ window.onload = () => {
     
     //EVENT LISTENERS
     document.addEventListener('keydown', (event) => {
-        if(event.keyCode === arrowKeys.left.code && !isSnakeRotating()) {
-            rotateSpeed =-20;
-            rotateTo += -90;
-        } 
-        if(event.keyCode === arrowKeys.right.code && !isSnakeRotating()) {
-            rotateSpeed =20;
-            rotateTo += 90;
-        } 
-        if(event.keyCode === arrowKeys.up.code && !isSnakeRotating()) {
-        } 
-        if(event.keyCode === arrowKeys.down.code && !isSnakeRotating()) {
-        } 
+
+        if(snake.facingDirection === DIRECTIONS.RIGHT) {
+            if(event.keyCode === arrowKeys.up.code && !isSnakeRotating()) {
+                rotateSpeed =-20;
+                rotateTo += -90;
+
+                snake.facingDirection = DIRECTIONS.UP;
+            } 
+            if(event.keyCode === arrowKeys.down.code && !isSnakeRotating()) {
+                rotateSpeed =20;
+                rotateTo += 90;
+
+                snake.facingDirection = DIRECTIONS.DOWN;
+            } 
+        }
+
+        if(snake.facingDirection === DIRECTIONS.LEFT) {
+            if(event.keyCode === arrowKeys.up.code && !isSnakeRotating()) {
+                rotateSpeed =20;
+                rotateTo += 90;
+
+                snake.facingDirection = DIRECTIONS.UP;
+            } 
+            if(event.keyCode === arrowKeys.down.code && !isSnakeRotating()) {
+                rotateSpeed =-20;
+                rotateTo += -90;
+
+                snake.facingDirection = DIRECTIONS.DOWN;
+            } 
+        }
+
+        if(snake.facingDirection === DIRECTIONS.UP) {
+            if(event.keyCode === arrowKeys.left.code && !isSnakeRotating()) {
+                rotateSpeed =-20;
+                rotateTo += -90;
+
+                snake.facingDirection = DIRECTIONS.LEFT;
+            } 
+            if(event.keyCode === arrowKeys.right.code && !isSnakeRotating()) {
+                rotateSpeed =20;
+                rotateTo += 90;
+
+                snake.facingDirection = DIRECTIONS.RIGHT;
+            }     
+        }
+
+        if(snake.facingDirection === DIRECTIONS.DOWN) {
+            if(event.keyCode === arrowKeys.left.code && !isSnakeRotating()) {
+                rotateSpeed =20;
+                rotateTo += 90;
+
+                snake.facingDirection = DIRECTIONS.LEFT;
+            } 
+            if(event.keyCode === arrowKeys.right.code && !isSnakeRotating()) {
+                rotateSpeed =-20;
+                rotateTo += -90;
+
+                snake.facingDirection = DIRECTIONS.RIGHT;
+            }     
+        }
+
+        //TESTING
+        if(event.keyCode === 32) {
+            console.log("Move");
+            moveSpeed = 1;
+        }
 
         //TESTING
         if(event.keyCode === 16) {
             growBody(1);
         }
+    });
+        //TESTING
+        document.addEventListener('keyup', (event) => {
+        if(event.keyCode === 32) {
+            moveSpeed = 0;
+        } 
     });
 }
 
