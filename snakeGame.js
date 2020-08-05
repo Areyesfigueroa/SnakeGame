640126051cd550b3b96bc193bd77213c706c5f4c
Aliel Reyes <alielreyes@gmail.com>
2020-08-04 21:45:04 -0700

    Experimenting with the snake body behaviour

--- snakeGame.js
+++ snakeGame.js
@@ -1,6 +1,20 @@
+
+//Factory Function
+const GameObject = (x, y, w, h) => {
+    
+    return {
+        posX: x,
+        posY: y,
+        width: w,
+        height: h
+    };
+}
+
+//CANVAS VARIABLES
 canvas = null;
 canvasContext = null;
 
+//SNAKE VARIABLES
 const DIRECTIONS = {
     RIGHT: 'right',
     LEFT: 'left',
@@ -20,18 +34,12 @@ const snake = {
     posY: 100,
     width: 50,
     height: 50,
-    unitLength: 1,
+    unitLength: 0,
     angle: 0,
     facingDirection: DIRECTIONS.RIGHT
 };
 
-const tile= {
-    posX: 0,
-    posY: 0,
-    style: '#B7E3BD',
-    width: snake.width,
-    height: snake.height
-}
+let snakeBodyParts = [];
 
 let image = new Image();
 image.src = './snake-sprite.png';
@@ -42,25 +50,19 @@ let moveSpeed=0;
 let nextTile = null;
 let latestInput = null;
 
-const gameBoard = {
-    columns: 0,
-    rows: 0,
-    coordinates: {
-        xAxis: [],
-        yAxis: []
-    }
-}
+//SNAKE DRAW METHODS
+const drawSnake = () => {
 
+    //Update Movement
+    updateObjMovementValues(snake);
 
+    //Update Rotation 
+    updateObjRotationValues(snake);
 
-const setArrowKeys = (active) => {
-    for(key in arrowKeys) {
-        arrowKeys[key].active = active;
-    }
-}
+    drawSnakeBody();
 
-const isSnakeRotating = () => {
-    return snake.angle !== rotateTo;
+    //Draw Snake and Update Rotation
+    rotate(rotateSpeed, rotateTo, snake);
 }
 
 const rotate = (angleSpeed, destAngle, drawingObj) => {
@@ -90,72 +92,180 @@ const rotate = (angleSpeed, destAngle, drawingObj) => {
     // canvasContext.drawImage(image, drawingObj.posX, drawingObj.posY, drawingObj.width, drawingObj.height);
 }
 
-const drawSnake = () => {
+const drawSnakeBody = () => {
 
+    if(!snake.unitLength) return;
+
+    for(let i = 0; i < snakeBodyParts.length; i++) {
+        //Update movements
+        updateObjMovementValues(snakeBodyParts[i]);
+
+        //Update our rotations
+        // updateObjRotationValues(snakeBodyParts[i]);
+
+        canvasContext.fillStyle = "purple";
+        canvasContext.fillRect(snakeBodyParts[i].posX, snakeBodyParts[i].posY, snakeBodyParts[i].width, snakeBodyParts[i].height);
+    }
+}
+
+const updateSnakeBodyRotationValues = () => {
+
+    // //Update rotation values based on facing direction.
+    // switch(latestInput) {
+    //     case arrowKeys.up.code:
+    //         rotateSpeed = obj.facingDirection === DIRECTIONS.RIGHT ? -20:20;
+    //         rotateTo += obj.facingDirection === DIRECTIONS.RIGHT ? -90:90;
+
+    //         obj.facingDirection = DIRECTIONS.UP;
+    //         break;
+    //     case arrowKeys.down.code:
+    //         rotateSpeed = obj.facingDirection === DIRECTIONS.RIGHT ? 20: -20;
+    //         rotateTo += obj.facingDirection === DIRECTIONS.RIGHT ? 90: -90;
+
+    //         obj.facingDirection = DIRECTIONS.DOWN;
+    //         break;
+    //     case arrowKeys.left.code:
+    //         rotateSpeed = obj.facingDirection === DIRECTIONS.UP ? -20:20;
+    //         rotateTo += obj.facingDirection === DIRECTIONS.UP ? -90: 90;
+
+    //         obj.facingDirection = DIRECTIONS.LEFT;
+    //         break;
+    //     case arrowKeys.right.code:
+    //         rotateSpeed = obj.facingDirection === DIRECTIONS.UP ? 20:-20;
+    //         rotateTo += obj.facingDirection === DIRECTIONS.UP ? 90: -90;
+
+    //         obj.facingDirection = DIRECTIONS.RIGHT;
+    //         break;
+    // }
+
+}
+
+//SNAKE DATA HANDLER METHODS
+const updateObjRotationValues = (obj) => {
+    //Have reached
+    if(latestInput && hasReachedNextTile(obj) && !isSnakeRotating()) {
+        
+        //Update rotation values based on facing direction.
+        switch(latestInput) {
+            case arrowKeys.up.code:
+                rotateSpeed = obj.facingDirection === DIRECTIONS.RIGHT ? -20:20;
+                rotateTo += obj.facingDirection === DIRECTIONS.RIGHT ? -90:90;
+
+                obj.facingDirection = DIRECTIONS.UP;
+                break;
+            case arrowKeys.down.code:
+                rotateSpeed = obj.facingDirection === DIRECTIONS.RIGHT ? 20: -20;
+                rotateTo += obj.facingDirection === DIRECTIONS.RIGHT ? 90: -90;
+
+                obj.facingDirection = DIRECTIONS.DOWN;
+                break;
+            case arrowKeys.left.code:
+                rotateSpeed = obj.facingDirection === DIRECTIONS.UP ? -20:20;
+                rotateTo += obj.facingDirection === DIRECTIONS.UP ? -90: 90;
+
+                obj.facingDirection = DIRECTIONS.LEFT;
+                break;
+            case arrowKeys.right.code:
+                rotateSpeed = obj.facingDirection === DIRECTIONS.UP ? 20:-20;
+                rotateTo += obj.facingDirection === DIRECTIONS.UP ? 90: -90;
+
+                obj.facingDirection = DIRECTIONS.RIGHT;
+                break;
+        }
+
+        //Reset latest input.
+        latestInput = null;
+    }
+}
+
+const updateObjMovementValues = (obj) => {
     //Update Movement
-    switch(snake.facingDirection) {
+    switch(obj.facingDirection) {
         case DIRECTIONS.RIGHT:
-            snake.posX += moveSpeed;
+            obj.posX += moveSpeed;
             break;
         case DIRECTIONS.LEFT:
-            snake.posX -= moveSpeed;
+            obj.posX -= moveSpeed;
             break;
         case DIRECTIONS.DOWN:
-            snake.posY += moveSpeed;
+            obj.posY += moveSpeed;
             break;
         case DIRECTIONS.UP:
-            snake.posY -= moveSpeed;
+            obj.posY -= moveSpeed;
             break;
         default:
             moveSpeed = 0;
     }
-
-    //Update Rotation
-    updateRotationValues();
-
-    //Snake Body
-    for(let i = 1; i < snake.unitLength; i++) {
-        canvasContext.fillStyle = "blue";
-        canvasContext.fillRect(snake.posX - ((snake.width)*i), snake.posY, snake.width, snake.height);
-    }
-    //Draw Snake and Update Rotation
-    rotate(rotateSpeed, rotateTo, snake);
 }
 
-const drawBoard = (rows, columns) => {
-    //Draw Board
-    let startingStyle = tile.style;
-    for(let row = 0; row < rows; row++) {
-        for(let col=0; col < columns; col++) {
-            canvasContext.fillStyle = tile.style;
-            canvasContext.fillRect(tile.posX + (tile.width * col),tile.posY + (tile.height * row),tile.width,tile.height);
-            
-            tile.style = tile.style === "#709E7C" ? "#B7E3BD":"#709E7C";
-        }
-        tile.posX = 0;
-        tile.style = startingStyle==="#B7E3BD" ? "#709E7C": "#B7E3BD";
-        startingStyle = tile.style;
+const updateSnakeNextTileValues = () => {
+
+    //Get the turning point coordinates so that we can draw them. 
+
+    //Update target
+    let idx = 0, direction = null;
+    switch(snake.facingDirection) {
+        case DIRECTIONS.UP:
+            idx = gameBoard.coordinates.yAxis.findIndex((yPlot) => yPlot > snake.posY) - 1;
+            nextTile = gameBoard.coordinates.yAxis[idx];
+
+            gameBoard.rotationTiles.push({posX: snake.posX, posY: nextTile});
+            break;
+        case DIRECTIONS.DOWN:
+            idx = gameBoard.coordinates.yAxis.findIndex((yPlot) => yPlot > snake.posY); 
+            nextTile = gameBoard.coordinates.yAxis[idx];
+
+            gameBoard.rotationTiles.push({posX: snake.posX, posY: nextTile});
+            break;
+        case DIRECTIONS.LEFT:
+            idx = gameBoard.coordinates.xAxis.findIndex((xPlot) => xPlot > snake.posX) - 1;
+            nextTile = gameBoard.coordinates.xAxis[idx];
+
+            gameBoard.rotationTiles.push({posX: nextTile - tile.height, posY: nextTile});
+            break;
+        case DIRECTIONS.RIGHT:
+            idx = gameBoard.coordinates.xAxis.findIndex((xPlot) => xPlot > snake.posX); 
+            nextTile = gameBoard.coordinates.xAxis[idx];
+            direction = DIRECTIONS.RIGHT;
+            break;
+        default:
+            console.log(`${snake.facingDirection} is not correct`);
+    }
+
+    //Update rotationTiles for snake body to use
+}
+
+const updateSnakeBodyValues = () => {
+    let newBodyPart = null;
+    switch(snake.facingDirection) {
+        case DIRECTIONS.RIGHT:
+            newBodyPart = GameObject(snake.posX - (snake.width * snake.unitLength), snake.posY, snake.width, snake.height);
+            newBodyPart.facingDirection = DIRECTIONS.RIGHT;
+            snakeBodyParts.push(newBodyPart);
+            break;
     }
 }
 
-const hasReachedNextTile = () => {
+
+//SNAKE HELPER METHODS
+const hasReachedNextTile = (obj) => {
     //There has to be a next tile and we have either gone past it on x or y axis. 
     if(!nextTile) return false;
 
-    if(snake.facingDirection === DIRECTIONS.LEFT) {
-        return snake.posX <= nextTile;
+    if(obj.facingDirection === DIRECTIONS.LEFT) {
+        return obj.posX <= nextTile;
     }
 
-    if(snake.facingDirection === DIRECTIONS.RIGHT) {
-        return snake.posX >= nextTile;
+    if(obj.facingDirection === DIRECTIONS.RIGHT) {
+        return obj.posX >= nextTile;
     }
 
-    if(snake.facingDirection === DIRECTIONS.UP) {
-        return snake.posY <= nextTile;
+    if(obj.facingDirection === DIRECTIONS.UP) {
+        return obj.posY <= nextTile;
     }
 
-    if(snake.facingDirection === DIRECTIONS.DOWN) {
-        return snake.posY >= nextTile;
+    if(obj.facingDirection === DIRECTIONS.DOWN) {
+        return obj.posY >= nextTile;
     }
 }
 
@@ -178,55 +288,35 @@ const isInputValid = (facingDirection, keyCode) => {
 
 }
 
-const updateRotationValues = () => {
-    //Have reached
-    if(latestInput && hasReachedNextTile() && !isSnakeRotating()) {
-        
-        //Update rotation values based on facing direction.
-        switch(latestInput) {
-            case arrowKeys.up.code:
-                rotateSpeed = snake.facingDirection === DIRECTIONS.RIGHT ? -20:20;
-                rotateTo += snake.facingDirection === DIRECTIONS.RIGHT ? -90:90;
-
-                snake.facingDirection = DIRECTIONS.UP;
-                break;
-            case arrowKeys.down.code:
-                rotateSpeed = snake.facingDirection === DIRECTIONS.RIGHT ? 20: -20;
-                rotateTo += snake.facingDirection === DIRECTIONS.RIGHT ? 90: -90;
-
-                snake.facingDirection = DIRECTIONS.DOWN;
-                break;
-            case arrowKeys.left.code:
-                rotateSpeed = snake.facingDirection === DIRECTIONS.UP ? -20:20;
-                rotateTo += snake.facingDirection === DIRECTIONS.UP ? -90: 90;
-
-                snake.facingDirection = DIRECTIONS.LEFT;
-                break;
-            case arrowKeys.right.code:
-                rotateSpeed = snake.facingDirection === DIRECTIONS.UP ? 20:-20;
-                rotateTo += snake.facingDirection === DIRECTIONS.UP ? 90: -90;
-
-                snake.facingDirection = DIRECTIONS.RIGHT;
-                break;
-        }
-
-        //Reset latest input.
-        latestInput = null;
-
-        //Update the next target.
-        nextTile = snake.posX + tile.width;
+const setArrowKeys = (active) => {
+    for(key in arrowKeys) {
+        arrowKeys[key].active = active;
     }
 }
 
-const drawNextTile = (x, y, w, h) => {
-    canvasContext.fillStyle = "blue";
-    canvasContext.fillRect(x, y, w, h);
+const isSnakeRotating = () => {
+    return snake.angle !== rotateTo;
 }
 
-window.onload = () => {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
-
+//GAME BOARD VARIABLES
+const gameBoard = {
+    columns: 0,
+    rows: 0,
+    coordinates: {
+        xAxis: [],
+        yAxis: []
+    },
+    rotationTiles: []
+}
+const tile= {
+    posX: 0,
+    posY: 0,
+    style: '#B7E3BD',
+    width: snake.width,
+    height: snake.height
+}
+//GAME BOARD METHODS
+const initializeGameBoardValues = (canvas) => {
     //Initialize Board
     gameBoard.columns = canvas.width/tile.width;
     gameBoard.rows = canvas.height/tile.height;
@@ -238,15 +328,55 @@ window.onload = () => {
     for(let i = 0; i <= gameBoard.columns; i++) {
         gameBoard.coordinates.xAxis.push(tile.width * i);
     } 
+}
+
+const drawBoard = (rows, columns) => {
+    //Draw Board
+    let startingStyle = tile.style;
+    for(let row = 0; row < rows; row++) {
+        for(let col=0; col < columns; col++) {
+            canvasContext.fillStyle = tile.style;
+            canvasContext.fillRect(tile.posX + (tile.width * col),tile.posY + (tile.height * row),tile.width,tile.height);
+            
+            tile.style = tile.style === "#709E7C" ? "#B7E3BD":"#709E7C";
+        }
+        tile.posX = 0;
+        tile.style = startingStyle==="#B7E3BD" ? "#709E7C": "#B7E3BD";
+        startingStyle = tile.style;
+    }
+}
+
+//GAME BOARD TESTING METHODS
+const drawTestCube = (x, y, w, h) => {
+    canvasContext.fillStyle = "blue";
+    canvasContext.fillRect(x, y, w, h);
+}
+
+const drawRotationTile = () => {
+    for(key in gameBoard.rotationTiles) {
+        drawTestCube(gameBoard.rotationTiles[key], snake.posY, 50, 50);
+
+        // drawTestCube(gameBoard.rotationTiles[key].posX, gameBoard.rotationTiles[key].posY, gameBoard.rotationTiles[key].width, gameBoard.rotationTiles[key].height);
+    }
+}
+
+//MAIN METHOD
+window.onload = () => {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+
+    initializeGameBoardValues(canvas);
 
     //Update Drawing
     const framesPerSecond = 60;
     setInterval(() => {    
         drawBoard(gameBoard.rows, gameBoard.columns);
         drawSnake();
+        drawRotationTile();
+
+
     }, 1000/framesPerSecond);
     
-
     //EVENT LISTENERS
     document.addEventListener('keydown', (event) => {
         
@@ -255,151 +385,21 @@ window.onload = () => {
             //Save latest input. 
             latestInput = event.keyCode;
 
-            //Update target
-            let idx = 0;
-            switch(snake.facingDirection) {
-                case DIRECTIONS.UP:
-                    idx = gameBoard.coordinates.yAxis.findIndex((yPlot) => yPlot > snake.posY) - 1;
-                    nextTile = gameBoard.coordinates.yAxis[idx];
-                    drawNextTile(snake.posX, gameBoard.coordinates.yAxis[idx], 10, 10);
-                    break;
-                case DIRECTIONS.DOWN:
-                    idx = gameBoard.coordinates.yAxis.findIndex((yPlot) => yPlot > snake.posY); 
-                    nextTile = gameBoard.coordinates.yAxis[idx];
-                    drawNextTile(snake.posX, gameBoard.coordinates.yAxis[idx], 10, 10);
-                    break;
-                case DIRECTIONS.LEFT:
-                    idx = gameBoard.coordinates.xAxis.findIndex((xPlot) => xPlot > snake.posX) - 1;
-                    nextTile = gameBoard.coordinates.xAxis[idx];
-                    drawNextTile(gameBoard.coordinates.xAxis[idx], snake.posY, 10, 10);
-                    break;
-                case DIRECTIONS.RIGHT:
-                    idx = gameBoard.coordinates.xAxis.findIndex((xPlot) => xPlot > snake.posX); 
-                    nextTile = gameBoard.coordinates.xAxis[idx];
-                    drawNextTile(gameBoard.coordinates.xAxis[idx], snake.posY, 10, 10);
-                    break;
-                default:
-                    console.log(`${latestInput} is not correct`);
-            }
-            console.log(nextTile);
+            updateSnakeNextTileValues();
             
         }
 
-        //TESTING
+        //TESTING - Spacebar
         if(event.keyCode === 32) {
             console.log("Move");            
-            moveSpeed = 3;
+            moveSpeed = moveSpeed ? 0:3;
         }
 
-        //TESTING
+        //TESTING - Shift
         if(event.keyCode === 16) {
+            //Update Snake Body
             snake.unitLength += 1;
+            updateSnakeBodyValues();
         }
     });
 }
-
-
-
-    // let posX=200, posY=200, width=200, height=200;
-
-    // canvasContext.save();
-    // canvasContext.translate(posX, posY);
-    // canvasContext.rotate(Math.PI/90);
-    // canvasContext.fillStyle = "blue";
-    // canvasContext.fillRect((-1*posX)/2,(-1*posY)/2,width, height);
-    // canvasContext.restore();
-
-        // document.addEventListener('keyup', (event) => {
-    //     if(event.keyCode === arrowKeys.left.code) {
-    //         // arrowKeys.left.active = false;
-    //     } 
-    //     if(event.keyCode === arrowKeys.right.code) {
-    //         arrowKeys.right.active = false;
-    //     } 
-    //     if(event.keyCode === arrowKeys.up.code) {
-    //         arrowKeys.up.active = false;
-    //     } 
-    //     if(event.keyCode === arrowKeys.down.code) {
-    //         arrowKeys.down.active = false;
-    //     } 
-    // });
-
-    // if(snake.facingDirection === DIRECTIONS.RIGHT) {
-
-    //     //Rotate once we reach the target
-    //     if(event.keyCode === arrowKeys.up.code && !isSnakeRotating()) {
-
-    //         //Update latest input
-    //         latestInput = DIRECTIONS.UP;
-
-    //         rotateSpeed =-20;
-    //         rotateTo += -90;
-
-    //         snake.facingDirection = DIRECTIONS.UP;
-    //     } 
-    //     if(event.keyCode === arrowKeys.down.code && !isSnakeRotating()) {
-    //         rotateSpeed =20;
-    //         rotateTo += 90;
-
-    //         snake.facingDirection = DIRECTIONS.DOWN;
-    //     } 
-    // }
-
-    // if(snake.facingDirection === DIRECTIONS.LEFT) {
-    //     if(event.keyCode === arrowKeys.up.code && !isSnakeRotating()) {
-    //         rotateSpeed =20;
-    //         rotateTo += 90;
-
-    //         snake.facingDirection = DIRECTIONS.UP;
-    //     } 
-    //     if(event.keyCode === arrowKeys.down.code && !isSnakeRotating()) {
-    //         rotateSpeed =-20;
-    //         rotateTo += -90;
-
-    //         snake.facingDirection = DIRECTIONS.DOWN;
-    //     } 
-    // }
-
-    // if(snake.facingDirection === DIRECTIONS.UP) {
-    //     if(event.keyCode === arrowKeys.left.code && !isSnakeRotating()) {
-    //         rotateSpeed =-20;
-    //         rotateTo += -90;
-
-    //         snake.facingDirection = DIRECTIONS.LEFT;
-    //     } 
-    //     if(event.keyCode === arrowKeys.right.code && !isSnakeRotating()) {
-    //         rotateSpeed =20;
-    //         rotateTo += 90;
-
-    //         snake.facingDirection = DIRECTIONS.RIGHT;
-    //     }     
-    // }
-
-    // if(snake.facingDirection === DIRECTIONS.DOWN) {
-    //     if(event.keyCode === arrowKeys.left.code && !isSnakeRotating()) {
-    //         rotateSpeed =20;
-    //         rotateTo += 90;
-
-    //         snake.facingDirection = DIRECTIONS.LEFT;
-    //     } 
-    //     if(event.keyCode === arrowKeys.right.code && !isSnakeRotating()) {
-    //         rotateSpeed =-20;
-    //         rotateTo += -90;
-
-    //         snake.facingDirection = DIRECTIONS.RIGHT;
-    //     }     
-    // }
-
-    // //TESTING
-    // if(event.keyCode === 32) {
-    //     console.log("Move");
-        
-    //     nextTile = snake.posX + tile.width;
-    //     moveSpeed = moveSpeed ? 0:1;
-    // }
-
-    // //TESTING
-    // if(event.keyCode === 16) {
-    //     snake.unitLength += 1;
-    // }
-        
